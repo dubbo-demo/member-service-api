1077cc781eaed2749f5a16e2dbbd6939a13c9d8a
xuxinpei <649087712@qq.com>
2017-09-15 17:37:45 +0800

    优化

---
+++ src/main/java/com/way/member/position/dto/PositionInfoDto.java
@@ -0,0 +1,28 @@
+package com.way.member.position.dto;
+
+import com.way.common.bean.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * 功能描述：定位信息Dto
+ *
+ * @Author：xinpei.xu
+ * @Date：2017/08/28 22:23
+ */
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class PositionInfoDto extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String phoneNo;// 手机号
+
+    private String longitude;// 经度
+
+    private String latitude;// 纬度
+}
---
+++ src/main/java/com/way/member/position/service/PositionInfoService.java
@@ -0,0 +1,11 @@
+package com.way.member.position.service;
+
+/**
+ * 功能描述：定位信息Service
+ *
+ * @Author：xinpei.xu
+ * @Date：2017/08/28 21:09
+ */
+public interface PositionInfoService {
+
+}
